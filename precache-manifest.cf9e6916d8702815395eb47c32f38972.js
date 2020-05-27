44802f8ad253173232f1a4f9dde264daade4ec20
Deric Parra <parradeko@gmail.com>
2020-05-27 19:58:13 -0300

    Updates

---
+++ precache-manifest.cf9e6916d8702815395eb47c32f38972.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6cd9b2b19e41a48dd7f0d407a6a54db",
+    "url": "/what-is-the-breed/index.html"
+  },
+  {
+    "revision": "c74f57e8ac1238ac89b8",
+    "url": "/what-is-the-breed/static/css/main.7db0969a.chunk.css"
+  },
+  {
+    "revision": "22a31470ec04fc0cb4be",
+    "url": "/what-is-the-breed/static/js/2.ee8a19de.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/what-is-the-breed/static/js/2.ee8a19de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c74f57e8ac1238ac89b8",
+    "url": "/what-is-the-breed/static/js/main.d9ff618f.chunk.js"
+  },
+  {
+    "revision": "db785222362955e46976",
+    "url": "/what-is-the-breed/static/js/runtime-main.00422f68.js"
+  }
+]);
